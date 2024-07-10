f6b9a98d2379af8c297de2622485f7c989df8bf7
yuritelis <mariana.garcia50@etec.sp.gov.br>
2024-07-09 21:59:27 -0300

    adição de imagens e edição das páginas do app

--- caoscruisermobile/lib/main.dart
+++ caoscruisermobile/lib/main.dart
@@ -69,7 +69,6 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context){
   return Scaffold(
-    backgroundColor: const Color.fromARGB(255, 34, 34, 34),
     body: Center(
         child: Container(
           decoration: const BoxDecoration(
@@ -77,21 +76,21 @@ class Home extends StatelessWidget{
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
               colors: [
-                Color.fromARGB(255, 243, 33, 33),
-                Color.fromARGB(193, 255, 171, 44),
+                Color.fromARGB(255, 26, 26, 26),
+                Color.fromARGB(193, 81, 81, 81),
               ],
             )
           ),
           child: Center(
             child: Column(
               children: [
-                Image.asset('img/capa.jpg', width: 310, height: 200,),
+                Image.asset('img/capa.png', width: 310, height: 200,),
                 const SizedBox(
                   width: 380,
                   child: Text('Caos Cruiser é um jogo de corrida retrô inspirado nos jogos de corrida clássico dos anos 80 a 90, com o objetivo de ultrapassar diversos carros em um período de um dia.',
                     style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: Colors.black,),
+                    color: Colors.white,),
                   ),
                 ),
                 const SizedBox(
@@ -99,7 +98,7 @@ class Home extends StatelessWidget{
                   child: Text('Se passa ao redor do mundo viajando por alguns países como Inglaterra, Brasil, Japão, Austrália e Egito, onde cada um tem sua estética de ambiente diferente, além de terem alguns fatores naturais únicos que o jogador enfrentará para vencer seus adversários. Com diversas opções de carros com diferentes habilidades especiais e estilos que os tornam mais incríveis e divertidos. Fique atento à pista e não relaxe pois será ultrapassado.',
                     style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: Colors.black,),
+                    color: Colors.white,),
                   ),
                 ),
               ]
@@ -116,18 +115,28 @@ class Game extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-  return const Scaffold(
-    backgroundColor: Color.fromARGB(255, 34, 34, 34),
+  return Scaffold(
     body: Center(
-        child: Column(
+      child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 26, 26, 26),
+                Color.fromARGB(193, 81, 81, 81),
+              ],
+            )
+          ),
+        child: const Column(
           children: <Widget>[
-            Text(
+             Text(
               'Jogo',
               style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
             ),
           ],
         ),
-      ),
+      ),)
   );
 }
 }
@@ -137,18 +146,29 @@ class Personagem extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-  return const Scaffold(
-    backgroundColor: Color.fromARGB(255, 34, 34, 34),
+  return Scaffold(
     body: Center(
+      child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 26, 26, 26),
+                Color.fromARGB(193, 81, 81, 81),
+              ],
+            )
+          ),
         child: Column(
           children: <Widget>[
-            Text(
+             const Text(
               'Personagem',
               style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
             ),
+            Image.asset('img/carro_gt40.png'),
           ],
         ),
-      ),
+      ),)
   );
 }
 }
@@ -158,18 +178,29 @@ class Cenario extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-  return const Scaffold(
-    backgroundColor: Color.fromARGB(255, 34, 34, 34),
+  return Scaffold(
     body: Center(
+      child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 26, 26, 26),
+                Color.fromARGB(193, 81, 81, 81),
+              ],
+            )
+          ),
         child: Column(
           children: <Widget>[
-            Text(
+            const Text(
               'Cenário',
               style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
             ),
+            Image.asset('img/scene_jp.png'),
           ],
         ),
-      ),
+      ),)
   );
 }
 }
