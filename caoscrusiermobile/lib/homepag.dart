be8ba067eee72dc8aaf63f17d2dbc584b506ae16
3JD <3jd@bglab.local>
2024-11-08 09:16:37 -0300

    edição da home

--- caoscrusiermobile/lib/homepag.dart
+++ caoscrusiermobile/lib/homepag.dart
@@ -60,87 +60,67 @@ class _HomePag extends State<HomePag> {
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
               colors: [
-                Color.fromARGB(255, 88, 0, 123),
-                Color.fromARGB(255, 57, 0, 71),
+                Color.fromARGB(255, 64, 4, 89),
+                Color.fromARGB(255, 14, 1, 55),
               ],
             )
           ),
-          child: Center(
+          child: Align(
             child: Container(
               decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(45)),
                 gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
                   colors: [
-                    Color.fromARGB(255, 64, 4, 89),
-                    Color.fromARGB(255, 14, 1, 55),
+                    Color.fromARGB(255, 88, 0, 123),
+                    Color.fromARGB(255, 57, 0, 71),
                   ],
                 )
               ),
-              child: Align(
-                child: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      colors: [
-                       Color.fromARGB(255, 64, 4, 89),
-                       Color.fromARGB(255, 14, 1, 55),
-                      ],
-                    )
+              child: Column(
+                children: [
+                  const SizedBox(
+                    width: 450,
                   ),
-                  child: Column(
-                    children: [
-                      Image.asset('img/capa.png', width: 310, height: 200,),
-                      const SizedBox(
-                        width: 380,
-                        child: Text(
-                          'Caos Cruiser é um jogo de corrida retrô inspirado nos jogos de corrida clássico dos anos 80 a 90, com o objetivo de ultrapassar diversos carros em um período de um dia.',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(
-                            fontFamily: 'ABeeZee',
-                            fontSize: 15,
-                            color: Colors.white,
-                          ),
-                        ),
+                  Image.asset('img/capa.png', width: 400, height: 380,),
+                  SizedBox(
+                    width: 380,
+                    child: Text(
+                      caosCrusier.sinopse,
+                      textAlign: TextAlign.justify,
+                      style: const TextStyle(
+                        fontFamily: 'ABeeZee',
+                        fontSize: 15,
+                        color: Colors.white,
                       ),
-                      const SizedBox(
-                        width: 380,
-                        child: Text(
-                          'Se passa ao redor do mundo viajando por alguns países como Inglaterra, Brasil, Japão, Austrália e Egito, onde cada um tem sua estética de ambiente diferente, além de terem alguns fatores naturais únicos que o jogador enfrentará para vencer seus adversários. Com diversas opções de carros com diferentes habilidades especiais e estilos que os tornam mais incríveis e divertidos. Fique atento à pista e não relaxe pois será ultrapassado.',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(
-                            fontFamily: 'ABeeZee',
-                            fontSize: 15,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      const Padding(padding: EdgeInsets.all(5)),
-                      SizedBox(
-                        height: 35,
-                        child: ElevatedButton(
-                          onPressed: () => navegacao(const GamePag()),
-                          style: const ButtonStyle(backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 255, 187, 0))),
-                          child: construirTitulo(title: caosCrusier.textosPag[1])
-                        ),
-                      ),
-                      const Padding(padding: EdgeInsets.all(5)),
-                      SizedBox(
-                        height: 35,
-                        child: ElevatedButton(
-                          onPressed: () => navegacao(const InspoPag()),
-                          style: const ButtonStyle(backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 255, 187, 0))),
-                          child: construirTitulo(title: caosCrusier.textosPag[5])
-                        ),
-                      ),
-                    ]
+                    ),
                   ),
-                ),
+                  const Padding(padding: EdgeInsets.all(10)),
+                  SizedBox(
+                    height: 35,
+                    child: ElevatedButton(
+                      onPressed: () => navegacao(const GamePag()),
+                      style: const ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 255, 187, 0))
+                      ),
+                      child: construirTitulo(title: caosCrusier.textosPag[1])
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  SizedBox(
+                    height: 35,
+                    child: ElevatedButton(
+                      onPressed: () => navegacao(const InspoPag()),
+                      style: const ButtonStyle(backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 255, 187, 0))),
+                      child: construirTitulo(title: caosCrusier.textosPag[5])
+                    ),
+                  ),
+                ]
               ),
-            )
+            ),
           ),
-        ),
+        )
       ),
     );
   }
